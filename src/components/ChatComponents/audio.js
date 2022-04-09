1b5d83dc8f27053f672908714b9634218eece571
yehuda <yehuda7701@gmail.com>
2022-04-09 16:06:27 +0300

    set audio to be good as video

--- src/components/ChatComponents/audio.js
+++ src/components/ChatComponents/audio.js
@@ -1,27 +1,31 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 
-const Record = ({ setAudioMsg }) => {
+const Audio = ({ setAudioMsg }) => {
     let setMsg = setAudioMsg
+
     const [stream, setStream] = useState({
         access: false,
         recorder: null,
         error: ""
     });
+    let [startClickable, setstartClickable] = useState(true);
+    let [stopClickable, setstopClickable] = useState(false);
 
     const [recording, setRecording] = useState({
         active: false,
         available: false,
         url: ""
     });
-
+    let preview = <audio id="audiopreview" srcObject={''} controls={false}></audio>
     const chunks = useRef([]);
-
+    useEffect(() => {
+        getAccess();
+    }, [])
     function getAccess() {
         navigator.mediaDevices
             .getUserMedia({ audio: true })
             .then((mic) => {
                 let mediaRecorder;
-
                 try {
                     mediaRecorder = new MediaRecorder(mic, {
                         mimeType: "audio/webm"
@@ -31,24 +35,37 @@ const Record = ({ setAudioMsg }) => {
                 }
 
                 const track = mediaRecorder.stream.getTracks()[0];
-                track.onended = () => console.log("ended");
+
+                track.onended = () => {
+                    console.log("ended");
+                }
 
                 mediaRecorder.onstart = function () {
+                    setstartClickable(false);
+                    setstopClickable(true);
                     setRecording({
                         active: true,
                         available: false,
                         url: ""
                     });
-                };
 
+
+                    let audio = document.querySelector("#audiopreview");
+                    //audio.srcObject = mic;
+                    audio.onloadedmetadata = function (e) {
+                        //audio.play();
+                    };
+
+
+                };
+                
                 mediaRecorder.ondataavailable = function (e) {
                     console.log("data available");
                     chunks.current.push(e.data);
                 };
-
                 mediaRecorder.onstop = function () {
+                    setstopClickable(false);
                     console.log("stopped");
-
                     const url = URL.createObjectURL(chunks.current[0]);
                     chunks.current = [];
 
@@ -57,6 +74,11 @@ const Record = ({ setAudioMsg }) => {
                         available: true,
                         url: url
                     });
+
+                    let audio = document.querySelector("#audiopreview");
+                    audio.srcObject = null
+                    audio.src = url;
+                    audio.controls = true
                     mediaRecorder.stream.getTracks().forEach(track => track.stop());
                     let comp = <audio key={recording.url} id="mulMedPrev" controls>
                         <source src={recording.url} type="audio" />
@@ -76,33 +98,34 @@ const Record = ({ setAudioMsg }) => {
                 setStream({ ...stream, error });
             });
     }
-
     return (
-        <div className="record">
-            {stream.access ? (
+        <div className="audio">
+            {(
                 <div className="audio-container">
                     <button
                         className={recording.active ? "active" : null}
-                        onClick={() => !recording.active && stream.recorder.start()}
+
+                        onClick={() => {
+                            if (startClickable) {
+                                stream.recorder.start();
+                            }
+                        }}
                     >
                         Start Recording
                     </button>
                     <button onClick={() => {
-                        stream.recorder.stop();
+                        if (stopClickable) {
+                            stream.recorder.stop();
+                        }
 
-                        // let comp = <audio key={recording.url} id="mulMedPrev" controls>
-                        //     <source src={recording.url} type="audio" />
-                        // </audio>;
-                        // console.log("recording.url", recording.url)
-                        // //while(!recording.available)
-                        // setAudioMsg({ msg: comp, content: recording.url, type: "audio" })
                     }}>Stop Recording</button>
-                    {recording.available && <audio controls src={recording.url} />}
+                    {preview}
+
                 </div>
-            ) : (
-                <button onClick={getAccess}>Get Mic Access</button>
-            )}
+            )
+            }
         </div>
+
     );
 }
-export default Record;
+export default Audio;
